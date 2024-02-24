cc154f7115e93b09c09d2cd0677077dc6df35452
BeSingularTeam <besingulardemo@gmail.com>
2024-02-24 23:45:08 +0530

    first commit

---
+++ Amazon-clone/src/Header.js
@@ -0,0 +1,89 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
+import categoriesData from "./data.json";
+import ArrowDropDown from "@material-ui/icons/ArrowDropDown";
+
+function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthenticaton = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__dropdown">
+    <div className="header__dropdownButton">
+      <span>All</span>
+      <ArrowDropDown />
+    </div>
+    <div className="header__dropdownContent"></div>
+      {/* Category List */}
+      <div className="header__categories">
+        <ul className="header__categoriesList">
+          {categoriesData.map((category, index) => (
+            <li key={index} className="header__category">
+              <Link to={`/src/data.json/${category}`} className="header__categoryLink">
+                {category}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      </div>
+
+
+      <div className="header__nav">
+        <Link to={!user && '/login'}>
+          <div onClick={handleAuthenticaton} className="header__option">
+            <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
+            <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
+          </div>
+        </Link>
+
+        <Link to='/orders'>
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
